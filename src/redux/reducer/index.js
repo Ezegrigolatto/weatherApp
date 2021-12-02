57d265f7c7db55ac1f8d82dbffbdaa184b01ffc5
Ezegrigolatto <ezegrigolatto@gmail.com>
2021-12-01 22:41:52 -0300

    v1.0

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,15 +1,9 @@
 const initialState = {
-    principal: [],
     allInfo: [],
 };
 
 export default function rootReducer(state = initialState, action) {
     switch (action.type) {
-        case 'GET_PRINCIPAL':
-            return {
-                ...state,
-                principal: action.payload,
-            };
         case 'GET_ALL_INFO':
             return {
                 ...state,
